f4fa4df2129eed643485742bed0b73319c5a90ab
angeliccodes <angel.njoku@selu.edu>
2026-02-08 05:25:20 -0600

    Added UserName and Password to LoginDto

--- Selu383.SP26.Api/Features/Authentication/LoginDto.cs
+++ Selu383.SP26.Api/Features/Authentication/LoginDto.cs
@@ -1,6 +1,8 @@
-﻿namespace Selu383.SP26.Api.Features.Authentication
-{
+﻿namespace Selu383.SP26.Api.Features.Authentication;
+
     public class LoginDto
     {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
-}
+
